703fec10cb28856c0cc48a2268b7c9282aedc991
gaurabpokharel <gaurab.pokharel@codehimalaya.com>
2023-08-05 12:02:51 -0400

    adding category in index.html

--- Quiz/admin.py
+++ Quiz/admin.py
@@ -1,5 +1,20 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import User
+from .models import *
+
 admin.site.register(User)
+
+admin.site.register(Category)
+
+
+class AnswerAdmin(admin.StackedInline):
+    model = Answer
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    inlines = [AnswerAdmin]
+
+
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Answer)
